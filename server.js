9182278df166a0a2e69db8772a26c9b1d7fa4cbe
Karan Manikani <karan.manikani@gmail.com>
2023-03-18 22:13:14 +0530

    Add routes for products

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 require("dotenv").config();
 const express = require("express");
 const connectToDB = require("./database/connectToDB");
+const productRouter = require("./routes/productRoutes");
+const errorHandler = require("./middleware/errorHandler");
 
 // Connect to DB
 connectToDB();
 
 const app = express();
+
+// Express middleware
 app.use(express.json());
+
+// Routes
+app.use("/api/products", productRouter);
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
